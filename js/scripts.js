fcb462d7f79a3c75bc52ea96b978ffcf108a6e56
Russvetsper <russvetsper@gmail.com>
2016-07-01 13:47:24 -0700

    atept number 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var totalCost;
-function Movie(meats,cheese,vegi,bread,sandwichPrice){
+function sandwich(meats,cheese,vegi,bread,sandwichPrice){
 this.title=meats;
 this.cheese=cheese;
 this.vegi=vegi;
@@ -7,20 +7,22 @@ this.bread=bread;
 this.sandwichPrice=sandwichPrice;
 
 }
-var pastrami=new sandwich("pastrami",15,10);
-var roastbeef=new sandwich("roastbeef",20,3);
-var cornbeef=new sandwich("cornbeef",,20,2);
+var pastrami=new sandwich("pastrami",15,20,30);
+var roastbeef=new sandwich("roastbeef"15,20,30);
+var cornbeef=new sandwich("cornbeef",15,20,30);
 
-//Moring ticket $10 more, afternoon ticket $15, evening ticket$20,night $0
+
 function sandwichAdditonalPrice(option){
   if(option==="1"){
     totalCost=totalCost+10;
   }else if(option==="2"){
-    totalCost=totalCost+100;//totalCost+=100
+    totalCost=totalCost+10;
   }else if(option==="3"){
-    totalCost+=200;
+    totalCost+=10;
   }else{
-    totalCost+=3;
+    totalCost+=5;
   }
 
-}
+  $(document).ready(function() {
+  $("form#user-form").submit(function(event){
+  event.preventDefault();
