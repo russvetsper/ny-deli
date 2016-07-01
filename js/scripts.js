cf3d8df7db1d53246e545eb6ee5bf151d8a73bb0
Russvetsper <russvetsper@gmail.com>
2016-07-01 11:11:35 -0700

    add new js function

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,18 @@ this.serving=bread;
 this.meats=meats;
 this.cheese=cheese;
 }
+var pastramiSandwich=new sandwich (20,30);
+var cornbeefSandwich=new sanwich (25,35);
+var turkeySandwich=new sanwich (15,20);
+var roastbeefSandwich=new sandwich (17,22);
+
+function sandwichAdditonalPrice(option){
+  if(option==="1"){
+    totalCost=totalCost+10;
+  }else if(option==="2"){
+    totalCost=totalCost+100;
+  }else if(option==="3"){
+    totalCost+=200;
+  }else{
+    totalCost+=3;
+  }
