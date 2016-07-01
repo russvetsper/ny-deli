8c0cb289e3f0f4ff9c90b298ff985d4798f9748c
Russvetsper <russvetsper@gmail.com>
2016-07-01 12:21:32 -0700

    add js if option

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-function Sandwich(sandwichname,cost,bread,meats,cheese){
-this.sandwichname=sandwichname;
-this.cost=cost;
-this.serving=bread;
-this.meats=meats;
+var totalCost;
+function Movie(meats,cheese,vegi,bread,sandwichPrice){
+this.title=meats;
 this.cheese=cheese;
-}
-var pastramiSandwich=new sandwich (20,30);
-var cornbeefSandwich=new sanwich (25,35);
-var turkeySandwich=new sanwich (15,20);
-var roastbeefSandwich=new sandwich (17,22);
+this.vegi=vegi;
+this.bread=bread;
+this.sandwichPrice=sandwichPrice;
 
+}
+var pastrami=new sandwich("pastrami",15,10);
+var roastbeef=new sandwich("roastbeef",20,3);
+var cornbeef=new sandwich("cornbeef",,20,2);
+
+//Moring ticket $10 more, afternoon ticket $15, evening ticket$20,night $0
 function sandwichAdditonalPrice(option){
   if(option==="1"){
     totalCost=totalCost+10;
   }else if(option==="2"){
-    totalCost=totalCost+100;
+    totalCost=totalCost+100;//totalCost+=100
   }else if(option==="3"){
     totalCost+=200;
   }else{
     totalCost+=3;
   }
 
-  $(document).ready(function() {
-  $("form#user-form").submit(function(event){
-  event.preventDefault();
+}
